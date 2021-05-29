a467c0aebdbde58aa2b12354bef949b6382a078f
theFl00f <celeste.ellerby@gmail.com>
2021-05-28 20:44:25 -0400

    remove hardcoded port

--- server/server.js
+++ server/server.js
@@ -55,8 +55,10 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.listen(3000, () => {
-  console.log("listening at port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
 });
 
 // error handler
